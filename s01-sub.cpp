788eb3a7d4a61e028df073c08ea4398db03810d7
s24530 <91981554+s24530@users.noreply.github.com>
2021-10-13 21:06:51 +0200

    Update s01-sub.cpp

--- s01-sub.cpp
+++ s01-sub.cpp
@@ -6,5 +6,5 @@ auto main( int argc, char* argv[]) -> int
     auto const a = std::stoi (argv[1]);
     auto const b = std::stoi (argv[2]);
     std::cout << (a-b) << "\n";
-    return o;
+    return 0;
 }
